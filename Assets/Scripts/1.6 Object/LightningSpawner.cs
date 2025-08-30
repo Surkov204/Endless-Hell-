7cd9b5b1243c9888729a51f6257f6d4211742780
DESKTOP-T9H4SPL\Admin <luongvantuananh111@gmail.com>
2025-08-30 18:05:44 +0700

    commit/update_lighting

---
+++ Assets/Scripts/1.6 Object/LightningSpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+public class LightningSpawner : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField] private GameObject warningPrefab;
+    [SerializeField] private GameObject lightningPrefab;
+
+    [Header("Spawn Settings")]
+    [SerializeField] private Transform lightningPoint;  
+    [SerializeField] private float spawnInterval = 3f;
+    [SerializeField] private float warningDuration = 1f;
+    [SerializeField] private float startSpawn = 1f;
+
+    private void Start()
+    {
+        Vector3 spawnPos = lightningPoint.position;
+        InvokeRepeating(nameof(SpawnLightning), startSpawn, spawnInterval);
+    }
+
+    private void SpawnLightning()
+    {
+        Vector3 spawnPos = lightningPoint.position;
+        spawnPos.z = 0.96f;
+        spawnPos.y = -2f;
+        GameObject warning = Instantiate(
+               warningPrefab,
+               spawnPos,
+               Quaternion.identity,
+               lightningPoint   
+           );
+
+        StartCoroutine(SpawnLightningAfterDelay(warning));
+    }
+
+    private IEnumerator SpawnLightningAfterDelay(GameObject warning)
+    {
+        yield return new WaitForSeconds(warningDuration);
+
+        Destroy(warning);
+        Vector3 spawnPos = lightningPoint.position;
+        spawnPos.z = 0.96f;
+        GameObject lightning = Instantiate(
+            lightningPrefab,
+            spawnPos,
+            Quaternion.identity,
+            lightningPoint  
+        );
+
+        Destroy(lightning, 0.5f); 
+    }
+}
